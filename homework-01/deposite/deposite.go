63f70c10d76c48a48efc7b3a9f71bae2b83bc999
b612lpp <asoinf@gmail.com>
2020-04-02 20:58:19 +0400

    commited smth

--- homework-01/deposite/deposite.go
+++ homework-01/deposite/deposite.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	var deposit float64
+	var percent float64
 	fmt.Println("Input deposit value")
-	fmt.Scan()
+	fmt.Scan(&deposit)
+	fmt.Println("which percent")
+	fmt.Scan(&percent)
 }
