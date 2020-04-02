08f1501049c29eea4aea0b6eb201920595c8c66c
b612lpp <asoinf@gmail.com>
2020-04-02 21:42:31 +0400

    * added

--- homework-01/deposite/deposite.go
+++ homework-01/deposite/deposite.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	var percent, deposit float64
 	fmt.Println("Input deposit value")
-	fmt.Scan()
+	fmt.Scan(&deposit)
+	fmt.Println("Input deposit percent")
+	fmt.Scan(&percent)
+	for i := 1; i < 6; i++ {
+		deposit = deposit + ((deposit * percent) / 100)
+		fmt.Println("after ", i, "year deposit gonna be ", deposit)
+	}
+
 }
